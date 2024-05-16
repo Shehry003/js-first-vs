a0983d3f4d57a5e00cf0aaa7933dcfe0c1d7cbcf
Shehry003 <shehryarnajam003@gmail.com>
2024-05-16 21:55:21 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,52 @@
+let score= null;
+
+console.log(typeof score);
+// // for number....................
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// //when we convert from one datatype toour desire datatype its also ahve limitations and rules..
+// //"33"=> 33
+// //"33abc"=>Nan(not a number)
+// //true=> 1; false=>0
+// // null=> 0;undefined=> Nan
+let isLoggedIn="true";
+// // for boolean...............
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1=> true: 0=> false
+// ""=> false: "hitesh"=> true
+let someNumber= 33;
+// for string........................
+// let stringsomenumber= String(someNumber);
+// console.log(stringsomenumber);
+// console.log(typeof stringsomenumber);
+
+//********************************************OPERATIONs ***********************************************
+let value= 3;
+let negvalue= -value;
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2%3);
+// console.log(2**3);..for power
+
+let str1="shehryar";
+let str2=" najam";
+let str3= str1+str2;
+console.log(str3);
+// some other cases................
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log((3+4)*5%3);
+// tricky behaviour not allowed in big companies.................
+// console.log(+true);
+// console.log(+"");
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+ let gamecounter=100
+ let Gamecounter= 100+
+ console.log(Gamecounter);
